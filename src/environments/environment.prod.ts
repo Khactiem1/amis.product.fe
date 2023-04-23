a905ea524cd58639b836ebd4360a420f8814948e
Khactiem1 <nguyenkhactiem2k1@gmail.com>
2023-04-23 08:56:57 +0700

    up

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,9 @@
 
 export const environment = {
     production: true,
-    IMAGE_API: 'http://tiem2603.duckdns.org:9996/assets',
-    BASE_API: 'http://tiem2603.duckdns.org:9996/api/v1',
+    // IMAGE_API: 'http://tiem2603.duckdns.org:9996/assets',
+    // BASE_API: 'http://tiem2603.duckdns.org:9996/api/v1',
 
-    // IMAGE_API: 'https://localhost:44349/assets',
-    // BASE_API: 'https://localhost:44349/api/v1',
+    IMAGE_API: 'https://localhost:44349/assets',
+    BASE_API: 'https://localhost:44349/api/v1',
 };
